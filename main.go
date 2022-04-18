e4caabb6a50f0e5f5b41a055bc73faab9477bec5
phpzc <zhang5474jj@163.com>
2022-04-18 18:33:17 +0800

    重构删除文章

--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"goblog/bootstrap"
 	"goblog/pkg/database"
-	"goblog/pkg/logger"
 	"net/http"
-	"strconv"
 	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -17,74 +14,6 @@ import (
 var router *mux.Router
 
 var db *sql.DB
-
-type Article struct {
-	Title, Body string
-	ID          int64
-}
-
-func (a Article) Delete() (rowsAffected int64, err error) {
-	rs, err := db.Exec("DELETE FROM articles WHERE id = " + strconv.FormatInt(a.ID, 10))
-
-	if err != nil {
-		return 0, err
-	}
-
-	if n, _ := rs.RowsAffected(); n > 0 {
-		return n, nil
-	}
-
-	return 0, nil
-}
-
-func getArticleByID(id string) (Article, error) {
-	article := Article{}
-	query := "SELECT * FROM articles WHERE id = ?"
-	err := db.QueryRow(query, id).Scan(&article.ID, &article.Title, &article.Body)
-	return article, err
-}
-
-func getRouteVariable(parameterName string, r *http.Request) string {
-	vars := mux.Vars(r)
-	return vars[parameterName]
-}
-
-func articlesDeleteHandler(w http.ResponseWriter, r *http.Request) {
-
-	id := getRouteVariable("id", r)
-
-	article, err := getArticleByID(id)
-
-	if err != nil {
-		if err == sql.ErrNoRows {
-			// 3.1 数据未找到
-			w.WriteHeader(http.StatusNotFound)
-			fmt.Fprint(w, "404 文章未找到")
-		} else {
-			// 3.2 数据库错误
-			logger.LogError(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprint(w, "500 服务器内部错误")
-		}
-	} else {
-		rowsAffected, err := article.Delete()
-
-		if err != nil {
-			logger.LogError(nil)
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprint(w, "500 服务器内部错误")
-		} else {
-
-			if rowsAffected > 0 {
-				indexURL, _ := router.Get("articles.index").URL()
-				http.Redirect(w, r, indexURL.String(), http.StatusFound)
-			} else {
-				w.WriteHeader(http.StatusNotFound)
-				fmt.Fprint(w, "404文章未找到")
-			}
-		}
-	}
-}
 
 func forceHTMLMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -115,8 +44,6 @@ func main() {
 
 	//router := http.NewServeMux()
 	//router := mux.NewRouter()
-
-	router.HandleFunc("/articles/{id:[0-9]+}/delete", articlesDeleteHandler).Methods("POST").Name("articles.delete")
 
 	//中间件 强制内容为HTML
 	router.Use(forceHTMLMiddleware)

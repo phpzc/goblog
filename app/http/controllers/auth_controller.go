ce44d8f588c4f09f52b6c56fffd3c564a737d723
phpzc <zhang5474jj@163.com>
2022-04-20 20:21:39 +0800

    注册表单验证

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"goblog/app/models/user"
+	"goblog/app/requests"
 	"goblog/pkg/view"
 	"net/http"
 )
@@ -18,22 +19,34 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
 //DoRegister
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 
-	name := r.PostFormValue("name")
-	email := r.PostFormValue("email")
-	password := r.PostFormValue("password")
-
+	//初始化数据
 	_user := user.User{
-		Name:     name,
-		Email:    email,
-		Password: password,
+		Name:            r.PostFormValue("name"),
+		Email:           r.PostFormValue("email"),
+		Password:        r.PostFormValue("password"),
+		PasswordConfirm: r.PostFormValue("password_confirm"),
 	}
 
-	_user.Create()
+	//表单规则
+	errs := requests.ValidateRegistrationForm(_user)
 
-	if _user.ID > 0 {
-		fmt.Fprint(w, "插入成功,ID为"+_user.GetStringID())
+	if len(errs) > 0 {
+
+		view.RenderSimple(w, view.D{
+			"Errors": errs,
+			"User":   _user,
+		}, "auth.register")
+
 	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, "创建用户失败，请联系管理员")
+		//验证成功
+		_user.Create()
+
+		if _user.ID > 0 {
+			http.Redirect(w, r, "/", http.StatusFound)
+
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "注册失败，请联系管理员")
+		}
 	}
 }

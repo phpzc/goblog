bd6c460cbf20306d6f3b99b6b8a97a0955c38dbb
phpzc <zhang5474jj@163.com>
2022-04-21 18:05:47 +0800

    确保用户名和邮箱唯一

---
+++ app/requests/request.go
@@ -0,0 +1,36 @@
+package requests
+
+import (
+	"errors"
+	"fmt"
+	"goblog/pkg/model"
+	"strings"
+
+	"github.com/thedevsaddam/govalidator"
+)
+
+func init() {
+
+	//自定义验证规则
+	govalidator.AddCustomRule("not_exists", func(field, rule, message string, value interface{}) error {
+		rng := strings.Split(strings.TrimPrefix(rule, "not_exists:"), ",")
+
+		tableName := rng[0]
+		dbFiled := rng[1]
+		val := value.(string)
+
+		var count int64
+		model.DB.Table(tableName).Where(dbFiled+" = ?", val).Count(&count)
+
+		if count != 0 {
+			if message != "" {
+				return errors.New(message)
+			}
+
+			return fmt.Errorf("%v 已被占用", val)
+		}
+
+		return nil
+	})
+
+}

58ffdff11e41b84ad4fec3bbe85c6eff14095234
phpzc <zhang5474jj@163.com>
2022-04-27 17:03:09 +0800

    创建分类

--- routes/web.go
+++ routes/web.go
@@ -47,6 +47,11 @@ func RegisterWebRoutes(r *mux.Router) {
 	uc := new(controllers.UserController)
 	r.HandleFunc("/users/{id:[0-9]+}", uc.Show).Methods("GET").Name("users.show")
 
+	//文章分类
+	cc := new(controllers.CategoriesController)
+	r.HandleFunc("/categories/create", middlewares.Auth(cc.Create)).Methods("GET").Name("categories.create")
+	r.HandleFunc("/categories", middlewares.Auth(cc.Store)).Methods("POST").Name("categories.store")
+
 	//静态文件
 	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
 	r.PathPrefix("/js/").Handler(http.FileServer(http.Dir("./public")))

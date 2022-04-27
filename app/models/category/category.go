20508787f9f771328f050ac2ff442347589bfd4d
phpzc <zhang5474jj@163.com>
2022-04-27 17:17:44 +0800

    关联文章和分类

--- app/models/category/category.go
+++ app/models/category/category.go
@@ -2,7 +2,9 @@ package category
 
 import (
 	"goblog/app/models"
+	"goblog/pkg/model"
 	"goblog/pkg/route"
+	"goblog/pkg/types"
 )
 
 // Category 文章分类
@@ -15,4 +17,16 @@ type Category struct {
 func (c Category) Link() string {
 
 	return route.Name2URL("categories.show", "id", c.GetStringID())
+}
+
+func Get(idstr string) (Category, error) {
+
+	var category Category
+	id := types.StringToUint64(idstr)
+	if err := model.DB.First(&category, id).Error; err != nil {
+		return category, err
+	}
+
+	return category, nil
+
 }

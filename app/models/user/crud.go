c71e40b38adf6f300b7bfac95c0cccda8b12e486
phpzc <zhang5474jj@163.com>
2022-04-27 16:13:36 +0800

    显示作者

--- app/models/user/crud.go
+++ app/models/user/crud.go
@@ -34,3 +34,13 @@ func GetByEmail(email string) (User, error) {
 	}
 	return user, nil
 }
+
+func All() ([]User, error) {
+
+	var users []User
+
+	if err := model.DB.Find(&users).Error; err != nil {
+		return users, err
+	}
+	return users, nil
+}
